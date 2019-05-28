5bd98b0ce664b438e774ddce24cb92f3154b48ca
Jhonatan Sandoval <jsandovalbazan@gmail.com>
2019-05-28 00:07:23 -0500

    primer commit

---
+++ src/app/notfoundpage/notfoundpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfoundpage',
+  templateUrl: './notfoundpage.component.html',
+  styles: []
+})
+export class NotfoundpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import {  OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styles: []
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
